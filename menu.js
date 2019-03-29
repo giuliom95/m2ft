30874b87ed4c3374a1edb2ca9dc2031d8a3f73c2
giuliom95 <giuliomartella1995@gmail.com>
2019-03-29 19:17:23 +0100

    Menu style

--- menu.js
+++ menu.js
@@ -4,51 +4,23 @@ const logo = document.createElement('img');
 logo.setAttribute('src', './logo.svg');
 menu.appendChild(logo);
 
-const home = document.createElement('div');
-if(page != 'home') {
-    const l = document.createElement('a');
-    l.textContent = 'home';
-    l.href = './home.html';
-    home.appendChild(l);
-} else home.textContent = 'home';
-menu.appendChild(home);
+entries = [
+    'home',
+    'architecture',
+    'research',
+    'about',
+    'contact'
+];
 
-
-const architecture = document.createElement('div');
-if(page != 'architecture') {
-    const l = document.createElement('a');
-    l.textContent = 'architecture';
-    l.href = './architecture.html';
-    architecture.appendChild(l);
-} else architecture.textContent = 'architecture';
-menu.appendChild(architecture);
-
-
-const research = document.createElement('div');
-if(page != 'research') {
-    const l = document.createElement('a');
-    l.textContent = 'research';
-    l.href = './research.html';
-    research.appendChild(l);
-} else research.textContent = 'research';
-menu.appendChild(research);
-
-
-const about = document.createElement('div');
-if(page != 'about') {
-    const l = document.createElement('a');
-    l.textContent = 'about';
-    l.href = './about.html';
-    about.appendChild(l);
-} else about.textContent = 'about';
-menu.appendChild(about);
-
-
-const contact = document.createElement('div');
-if(page != 'contact') {
-    const l = document.createElement('a');
-    l.textContent = 'contact';
-    l.href = './contact.html';
-    contact.appendChild(l);
-} else contact.textContent = 'contact';
-menu.appendChild(contact);
+entries.forEach(e => {
+    const div = document.createElement('div');
+    const span = document.createElement('span');
+    span.textContent = e;
+    if(page != e) {
+        const l = document.createElement('a');
+        l.href = './' + e + '.html';
+        l.appendChild(span);
+        div.appendChild(l);
+    } else div.appendChild(span);
+    menu.appendChild(div);
+});
